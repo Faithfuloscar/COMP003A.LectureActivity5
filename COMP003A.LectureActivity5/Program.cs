d9a012b2e361354fcbb0b1ce27366722236e14e1
oscar <oscar@LAPTOP-G1N9MG0D>
2024-02-13 23:46:41 -0800

    Add project files.

---
+++ COMP003A.LectureActivity5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Author: Oscar Mendez
+ * Course: COMP-003A
+ * Purpose: To replicate lecture activity 5
+ */
+
+namespace COMP003A.LectureActivity5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
